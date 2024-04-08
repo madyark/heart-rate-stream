922f51f28aa812df83091d27627e4335348fee05
madyark <kozhakhmetovmadyar@gmail.com>
2024-04-08 10:33:01 +0200

    Add assets daily scheduling job

--- orchestrate/__init__.py
+++ orchestrate/__init__.py
@@ -1,10 +1,18 @@
-from dagster import Definitions, load_assets_from_package_module
+from dagster import Definitions, define_asset_job, ScheduleDefinition, AssetSelection
 from orchestrate.assets.airbyte.airbyte import airbyte_assets_streaming
 from orchestrate.assets.dbt.dbt import dbt_warehouse, dbt_warehouse_resource
+
+run_everything_job = define_asset_job("run_everything", selection="*")
 
 defs = Definitions(
     assets=[airbyte_assets_streaming, dbt_warehouse],
     resources={
         "dbt_warehouse_resource": dbt_warehouse_resource
-    }
+    },
+    schedules=[
+        ScheduleDefinition(
+            job=run_everything_job,
+            cron_schedule="@daily",
+        ),
+    ],
 )
--- orchestrate/assets/airbyte/airbyte.py
+++ orchestrate/assets/airbyte/airbyte.py
@@ -11,6 +11,6 @@ airbyte_resource = AirbyteResource(
 airbyte_assets_streaming = load_assets_from_airbyte_instance(
     airbyte_resource,
     key_prefix="stream_data",
-    connection_to_freshness_policy_fn=lambda _: FreshnessPolicy(maximum_lag_minutes=60),
+    connection_to_freshness_policy_fn=lambda _: FreshnessPolicy(maximum_lag_minutes=1),
     connection_to_auto_materialize_policy_fn=lambda _: AutoMaterializePolicy.eager(),
 )
