5d956a0fd089dc15134fa262271afec7e05f701e
madyark <kozhakhmetovmadyar@gmail.com>
2024-04-08 03:50:57 +0200

    Change dbt folder name to transform

--- warehouse/macros/dob_transform.sql
+++
@@ -1,5 +0,0 @@
--- Transforms date of birth values from YYYYMMDD in int format to YYYY-MM-DD in varchar
-
-{% macro dob_transform(dob_column) %}
-    to_date(cast({{ dob_column }} as varchar), 'YYYYMMDD') as {{ dob_column }}
-{% endmacro %}
--- warehouse/models/serving/dim_dates.sql
+++
@@ -1,3 +0,0 @@
--- Dates reference data (to be loaded once and persisted)
-
-{{ dbt_date.get_date_dimension(start_date="1900-01-01", end_date="2099-12-31") }}
