049f30904dc4218722de4eb48910d3e9deca78f7
madyark <kozhakhmetovmadyar@gmail.com>
2024-04-20 16:57:48 +0200

    Remove explicit asset job scheduler from orchestrate/__init__.py

--- orchestrate/__init__.py
+++ orchestrate/__init__.py
@@ -1,18 +1,10 @@
-from dagster import Definitions, define_asset_job, ScheduleDefinition, AssetSelection
+from dagster import Definitions, ScheduleDefinition, AssetSelection
 from orchestrate.assets.airbyte.airbyte import airbyte_assets_streaming
 from orchestrate.assets.dbt.dbt import dbt_warehouse, dbt_warehouse_resource
-
-run_everything_job = define_asset_job("run_everything", selection="*")
 
 defs = Definitions(
     assets=[airbyte_assets_streaming, dbt_warehouse],
     resources={
         "dbt_warehouse_resource": dbt_warehouse_resource
     },
-    schedules=[
-        ScheduleDefinition(
-            job=run_everything_job,
-            cron_schedule="@daily",
-        ),
-    ],
 )
