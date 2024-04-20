7882df7c20cded9a43f1221747b2ad1fae113d52
madyark <kozhakhmetovmadyar@gmail.com>
2024-04-20 15:28:47 +0200

    Change key_prefix value in airbyte.py

--- orchestrate/assets/airbyte/airbyte.py
+++ orchestrate/assets/airbyte/airbyte.py
@@ -10,7 +10,7 @@ airbyte_resource = AirbyteResource(
 
 airbyte_assets_streaming = load_assets_from_airbyte_instance(
     airbyte_resource,
-    key_prefix="stream_data",
+    key_prefix="raw",
     connection_to_freshness_policy_fn=lambda _: FreshnessPolicy(maximum_lag_minutes=60),
     connection_to_auto_materialize_policy_fn=lambda _: AutoMaterializePolicy.eager(),
 )
