6cbf3c5ee35dc2d2efa95f2c1946ee4f1fd0c3a3
madyark <kozhakhmetovmadyar@gmail.com>
2024-04-21 15:58:18 +0200

    Change cron schedule value in airbyte.py from 1AM UTC to 1:30AM UTC

--- orchestrate/assets/airbyte/airbyte.py
+++ orchestrate/assets/airbyte/airbyte.py
@@ -11,6 +11,6 @@ airbyte_resource = AirbyteResource(
 airbyte_assets_streaming = load_assets_from_airbyte_instance(
     airbyte_resource,
     key_prefix="raw",
-    connection_to_freshness_policy_fn=lambda _: FreshnessPolicy(maximum_lag_minutes=1, cron_schedule="0 1 * * *"),
+    connection_to_freshness_policy_fn=lambda _: FreshnessPolicy(maximum_lag_minutes=1, cron_schedule="30 1 * * *"),
     connection_to_auto_materialize_policy_fn=lambda _: AutoMaterializePolicy.eager(),
 )
