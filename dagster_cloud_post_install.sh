2f1f23c43c486b0ff6640022c0fa413cc5d7c91e
madyark <kozhakhmetovmadyar@gmail.com>
2024-04-22 16:45:31 +0200

    Add comment to make shell file executable and pass to git

--- dagster_cloud_post_install.sh
+++ dagster_cloud_post_install.sh
@@ -1,1 +1,4 @@
+# Installs dbt dependencies when deploying to Dagster Cloud Serverless 
+# https://github.com/dagster-io/dagster/discussions/12180
+
 dbt deps --project-dir ./transform/ --target prod
