1edd9128fde128f91061a770d325af102553d3ee
madyark <kozhakhmetovmadyar@gmail.com>
2024-04-22 14:08:26 +0200

    Revert changes in deploy.yml and dbt.py as fix unsuccessful

--- orchestrate/assets/dbt/dbt.py
+++ orchestrate/assets/dbt/dbt.py
@@ -8,6 +8,9 @@ from dagster_dbt import DbtCliResource, dbt_assets
 dbt_project_dir = Path(__file__).joinpath("..", "..", "..", "..", "transform").resolve()
 dbt_warehouse_resource = DbtCliResource(project_dir=os.fspath(dbt_project_dir))
 
+# Run dbt deps
+dbt_warehouse_resource.cli(["deps"]).wait()
+
 # Generate manifest.json file
 dbt_manifest_path = (
     dbt_warehouse_resource.cli(
